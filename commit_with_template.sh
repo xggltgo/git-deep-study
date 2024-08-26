0f2d5faaaa3e69dbbdbaa8af005839e221a2987d
xgglt <1799583372@qq.com>
2024-08-26 23:21:01 +0800

    55sd

--- commit_with_template.sh
+++ commit_with_template.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # 提交模板文件
-TEMPLATE_FILE="git-commit-tempalte.txt"
+TEMPLATE_FILE="git-commit-template.txt"
 
 # 读取模板内容
-template=$(cat "$TEMPLATE_FILE")
+if [ -f "$TEMPLATE_FILE" ]; then
+  template=$(cat "$TEMPLATE_FILE")
+else
+  echo "Template file not found: $TEMPLATE_FILE"
+  exit 1
+fi
 
 # 从命令行获取提交信息
 commit_message="$*"
 
 # 合并模板内容和提交信息
-combined_message="$template\n\n$commit_message"
+combined_message=$(printf "%s\n\n%s" "$template" "$commit_message")
+
+# 输出合并后的消息用于调试
+echo "Combined commit message:"
+echo "$combined_message"
 
 # 执行 git commit
 git commit -m "$combined_message"
